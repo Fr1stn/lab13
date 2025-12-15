a97b13247c8d828bccc984dd5d78411017b9ca03
Fr1stn <happipampers00@gmail.com>
2025-12-15 12:55:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число:");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+double b = Convert.ToDouble(Console.ReadLine());
+try
+{
+    double s = a / b;
+    Console.WriteLine(s);
+}
+catch(DivideByZeroException)
+{
+    if (b == 0)
+    {
+        Console.WriteLine("Ошибка при делении на 0.");
+    }
+    else if (a == 0 && b == 0)
+    {
+        Console.WriteLine("Ошибка!");
+    }
+}
